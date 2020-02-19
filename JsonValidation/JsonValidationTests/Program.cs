2a97fd20bcc4e4a9a0a477e9a499dca950d044c0
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2020-02-19 19:22:08 +0200

    JsonString
    Modified and working

---
+++ JsonValidation/JsonValidationTests/Program.cs
@@ -0,0 +1,101 @@
+﻿sing System;
+
+namespace JsonValidation
+{
+    public class Program
+    {
+        public static bool ValidateJsonString(string json)
+        {
+            if (json == null)
+            {
+                throw new ArgumentNullException(json);
+            }
+
+            return CheckForBorders(json) && CheckForInvalidCharacters(json);
+        }
+
+        private static bool CheckForUnicode(string json)
+        {
+            if (json == null)
+            {
+                throw new ArgumentNullException(json);
+            }
+
+            if (json.IndexOf("\\u") == -1)
+            {
+                return false;
+            }
+
+            int index = json.IndexOf("\\u");
+            if (json.Length < index + 5)
+            {
+                return false;
+            }
+
+            for (int i = index + 2; i <= index + 5; i++)
+            {
+                if ((json[i] < '0' && json[i] > '9') && (json[i] < 'A' && json[i] > 'Z'))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool CheckForInvalidCharacters(string json)
+        {
+            const string invalidCharacters = "\"\\";
+
+            for (int i = 1; i < json.Length - 1; i++)
+            {
+                if (char.IsControl(json[i]) && !CheckForException(json[i]))
+                {
+                    return false;
+                }
+
+                if (!CheckForUnicode(json) && invalidCharacters.IndexOf(json[i]) != -1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool CheckForException(char json)
+        {
+            switch (json)
+            {
+                case '\n':
+
+                case '\\':
+
+                case '\b':
+
+                case '\f':
+
+                case '\r':
+
+                case '\t':
+
+                case '\"':
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool CheckForBorders(string json)
+        {
+            if (json.Length < 2)
+            {
+                return false;
+            }
+
+            return json[0] == '"' && json[json.Length - 1] == '"';
+        }
+    }
+}
---
+++ JsonValidation/JsonValidationTests/UnitTest1.cs
@@ -0,0 +1,71 @@
+using JsonValidation;
+using Xunit;
+
+namespace JsonValidationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void InvalidatesStringBecauseIsNotBorderedByQuotationMarks()
+        {
+            const string testCase = "testCase";
+            Assert.False(Program.ValidateJsonString(testCase));
+        }
+
+        [Fact]
+
+        public void InvalidateStringsBecauseIsBorderedOnlyOnLeftSide()
+        {
+            const string testCase = "\"testCase";
+            Assert.False(Program.ValidateJsonString(testCase));
+        }
+
+        [Fact]
+
+        public void InvalidatesStringBecauseIsBorderedOnlyOnRightSide()
+        {
+            const string testCase = "testCase\"";
+            Assert.False(Program.ValidateJsonString(testCase));
+        }
+
+        [Fact]
+
+        public void InvalidatesStringBecauseItContainsBackslashes()
+        {
+            const string testCase = "\"t\\estCase\"";
+            Assert.False(Program.ValidateJsonString(testCase));
+        }
+
+        [Fact]
+
+        public void InvalidatesStringBecauseItContainsQuotes()
+        {
+            const string testCase = "\"Te\"st\"";
+            Assert.False(Program.ValidateJsonString(testCase));
+        }
+
+        [Fact]
+
+        public void InvalidatesSingleCharacterStringContainingInvalidCharacter()
+        {
+            const string testCase = "\\";
+            Assert.False(Program.ValidateJsonString(testCase));
+        }
+
+        [Fact]
+
+        public void ValidatesSimpleCorrectJsonString()
+        {
+            const string testCase = "\"Test\"";
+            Assert.True(Program.ValidateJsonString(testCase));
+        }
+
+        [Fact]
+
+        public void ValidatesComplexCorrectJsonString()
+        {
+            const string testCase = "\"Test\\u0097\nAnother line\"";
+            Assert.True(Program.ValidateJsonString(testCase));
+        }
+    }
+}
