5dc4c4b3f05d73fab851ef67467eb4fe736fb647
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2020-03-25 23:29:46 +0200

    Many class + tests

---
+++ range/Range/Many.cs
@@ -0,0 +1,33 @@
+﻿namespace Range
+{
+    public class Many : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Many(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return (IMatch)new SuccesMatch(text);
+            }
+
+            IMatch match = new SuccesMatch(text);
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!match.Success())
+                {
+                    break;
+                }
+
+                match = pattern.Match(match.RemainingText());
+            }
+
+            return (IMatch)new SuccesMatch(match.RemainingText());
+        }
+    }
+}
---
+++ range/Range/ManyTests.cs
@@ -0,0 +1,63 @@
+﻿using Xunit;
+
+namespace Range
+{
+    public class ManyTests
+    {
+        [Fact]
+        public void ConsumesOneOccurenceOfPatternInText()
+        {
+            var a = new Many(new Character('a'));
+            Assert.True(a.Match("abc").Success());
+            Assert.Equal("bc", a.Match("abc").RemainingText());
+        }
+
+        [Fact]
+        public void ConsumesAllOccurencesOfPatternInText()
+        {
+            var a = new Many(new Character('a'));
+            Assert.True(a.Match("aaaabc").Success());
+            Assert.Equal("bc", a.Match("aaaabc").RemainingText());
+        }
+
+        [Fact]
+        public void DoesNotConsumePatternWhenNotInText()
+        {
+            var a = new Many(new Character('a'));
+            Assert.True(a.Match("bc").Success());
+            Assert.Equal("bc", a.Match("bc").RemainingText());
+        }
+
+        [Fact]
+        public void DoesNotConsumePatternInEmptyText()
+        {
+            var a = new Many(new Character('a'));
+            Assert.True(a.Match("").Success());
+            Assert.Equal("", a.Match("").RemainingText());
+        }
+
+        [Fact]
+        public void DoesNotConsumePatternInNullText()
+        {
+            var a = new Many(new Character('a'));
+            Assert.True(a.Match(null).Success());
+            Assert.Null(a.Match(null).RemainingText());
+        }
+
+        [Fact]
+        public void ConsumesMultipleCharactersInRange()
+        {
+            var digits = new Many(new Range('0', '9'));
+            Assert.True(digits.Match("12345ab123").Success());
+            Assert.Equal("ab123", digits.Match("12345ab123").RemainingText());
+        }
+
+        [Fact]
+        public void DoesNotConsumeAnyCharacterBecauseNoneIsInRange()
+        {
+            var digits = new Many(new Range('0', '9'));
+            Assert.True(digits.Match("ab").Success());
+            Assert.Equal("ab", digits.Match("ab").RemainingText());
+        }
+    }
+}
