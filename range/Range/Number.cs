1a148854b7ae526c83c3923f411c8ff4b44db3b9
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2020-04-13 19:22:44 +0300

    Number class + tests

---
+++ range/Range/Number.cs
@@ -0,0 +1,23 @@
+﻿namespace Range
+{
+    public class Number : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Number()
+        {
+            var digits = new Many(new Range('0', '9'));
+            var signs = new Any("+-");
+            var exponents = new Any("eE");
+            var integerSide = new Sequence(new Optional(new Character('-')), new Sequence(digits));
+            var exponentSide = new Optional(new Sequence(exponents, new Optional(signs), new Range('1', '9')));
+            var rationalSide = new Optional(new Sequence(new Character('.'), digits));
+            pattern = new Sequence(integerSide, rationalSide, exponentSide);
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ range/Range/NumberTests.cs
@@ -0,0 +1,95 @@
+﻿using Xunit;
+
+namespace Range
+{
+    public class NumberTests
+    {
+        [Fact]
+        public void ValidatesUnsignedIntegerNumber()
+        {
+            var number = new Number();
+            Assert.True(number.Match("1234").Success());
+            Assert.Equal("", number.Match("1234").RemainingText());
+        }
+
+        [Fact]
+        public void ValidatesUnsignedZeroStartingIntegerNumber()
+        {
+            var number = new Number();
+            Assert.True(number.Match("01234").Success());
+            Assert.Equal("", number.Match("01234").RemainingText());
+        }
+
+        [Fact]
+        public void ValidatesSignedIntegerNumber()
+        {
+            var number = new Number();
+            Assert.True(number.Match("-1234").Success());
+            Assert.Equal("", number.Match("-1234").RemainingText());
+        }
+
+        [Fact]
+        public void ValidatesUnsignedRationalNumber()
+        {
+            var number = new Number();
+            Assert.True(number.Match("1.234").Success());
+            Assert.Equal("", number.Match("1.234").RemainingText());
+        }
+
+        [Fact]
+        public void ValidatesSignedRationalNumber()
+        {
+            var number = new Number();
+            Assert.True(number.Match("-1.234").Success());
+            Assert.Equal("", number.Match("-1.234").RemainingText());
+        }
+
+        [Fact]
+        public void ValidatesIncompleteExponentialFormatNumber()
+        {
+            var number = new Number();
+            Assert.True(number.Match("12.123E").Success());
+            Assert.Equal("E", number.Match("12.123E").RemainingText());
+        }
+
+        [Fact]
+        public void ValidatesIncompleteRationalNumber()
+        {
+            var number = new Number();
+            Assert.True(number.Match("123.").Success());
+            Assert.Equal("", number.Match("123.").RemainingText());
+        }
+
+        [Fact]
+        public void ValidatesLowerCasePositiveExponentNumber()
+        {
+            var number = new Number();
+            Assert.True(number.Match("123.12e+1").Success());
+            Assert.Equal("", number.Match("123.12e+1").RemainingText());
+        }
+
+        [Fact]
+        public void ValidatesUpperCasePositiveExponentNumber()
+        {
+            var number = new Number();
+            Assert.True(number.Match("123.12E+1").Success());
+            Assert.Equal("", number.Match("123.12E+1").RemainingText());
+        }
+
+        [Fact]
+        public void ValidatesLowerCaseNegativeExponentNumber()
+        {
+            var number = new Number();
+            Assert.True(number.Match("123.12e-1").Success());
+            Assert.Equal("", number.Match("123.12e-1").RemainingText());
+        }
+
+        [Fact]
+        public void ValidatesUpperCaseNegativeExponentNumber()
+        {
+            var number = new Number();
+            Assert.True(number.Match("123.12E-1").Success());
+            Assert.Equal("", number.Match("123.12E-1").RemainingText());
+        }
+    }
+}
