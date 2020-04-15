73e015c5b44301c04fdc8495a067881ea42200d7
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2020-04-15 16:35:39 +0300

    Support for 0 starting numbers

--- range/Range/Number.cs
+++ range/Range/Number.cs
@@ -9,7 +9,7 @@
             var digits = new Many(new Range('0', '9'));
             var signs = new Any("+-");
             var exponents = new Any("eE");
-            var integerSide = new Sequence(new Optional(new Character('-')), new Sequence(digits));
+            var integerSide = new Choice(new Character('0'), new Sequence(new Optional(new Character('-')), new Sequence(digits)));
             var exponentSide = new Optional(new Sequence(exponents, new Optional(signs), new Range('1', '9')));
             var rationalSide = new Optional(new Sequence(new Character('.'), digits));
             pattern = new Sequence(integerSide, rationalSide, exponentSide);
