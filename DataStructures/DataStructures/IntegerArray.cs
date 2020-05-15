9e1a9c4c803d53b2ca9e2be4f41863eebbaef9d7
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2020-05-15 23:59:52 +0300

    IntArray class + tests

---
+++ DataStructures/DataStructures/IntegerArray.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class IntArray
+    {
+        private int[] array;
+        private int count;
+
+        public IntArray()
+        {
+            this.array = new int[4];
+            this.count = 0;
+        }
+
+        public void Add(int element)
+        {
+            if (count == array.Length)
+            {
+                Array.Resize(ref array, array.Length * 2);
+            }
+
+            array[count] = element;
+            count++;
+        }
+
+        public int Count()
+        {
+            return this.count;
+        }
+
+        public int Element(int index)
+        {
+            return this.array[index];
+        }
+
+        public void SetElement(int index, int element)
+        {
+            this.array[index] = element;
+        }
+
+        public bool Contains(int element)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (array[i].Equals(element))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public int IndexOf(int element)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (array[i] == element)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, int element)
+        {
+            if (count == array.Length)
+            {
+                Array.Resize(ref array, array.Length * 2);
+            }
+
+            for (int i = count; i > index; i--)
+            {
+                array[i] = array[i - 1];
+            }
+
+            array[index] = element;
+            count++;
+        }
+
+        public void Clear()
+        {
+            this.count = 0;
+        }
+
+        public void Remove(int element)
+        {
+            int index = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (array[i].Equals(element))
+                {
+                    index = i;
+                }
+            }
+
+            RemoveAt(index);
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int i = count - 1; i > index; i--)
+            {
+                array[i - 1] = array[i];
+            }
+
+            count--;
+        }
+    }
+}
---
+++ DataStructures/DataStructures/IntegerArrayTests.cs
@@ -0,0 +1,134 @@
+﻿using Xunit;
+
+namespace DataStructures
+{
+    public class IntegerArrayTests
+    {
+        [Fact]
+        public void ValidatesConstructor()
+        {
+            var test = new IntArray();
+            Assert.Equal(0, test.Count());
+            Assert.Equal(0, test.Element(0));
+            Assert.Equal(0, test.Element(1));
+            Assert.Equal(0, test.Element(2));
+            Assert.Equal(0, test.Element(3));
+        }
+
+        [Fact]
+        public void ValidatesAddMethod()
+        {
+            var test = new IntArray();
+            test.Add(5);
+            test.Add(3);
+            test.Add(2);
+            test.Add(6);
+            test.Add(7);
+            Assert.Equal(5, test.Element(0));
+            Assert.Equal(3, test.Element(1));
+            Assert.Equal(2, test.Element(2));
+            Assert.Equal(6, test.Element(3));
+            Assert.Equal(7, test.Element(4));
+        }
+
+        [Fact]
+        public void ValidatesCountMethod()
+        {
+            var test = new IntArray();
+            test.Add(5);
+            test.Add(3);
+            test.Add(2);
+            test.Add(6);
+            test.Add(7);
+            Assert.Equal(5, test.Count());
+        }
+
+        [Fact]
+        public void ValidatesSetElementMethod()
+        {
+            var test = new IntArray();
+            test.Add(5);
+            test.SetElement(0, 2);
+            Assert.Equal(2, test.Element(0));
+        }
+
+        [Fact]
+        public void ValidatesContainMethodAndFindsElement()
+        {
+            var test = new IntArray();
+            test.Add(5);
+            test.Add(3);
+            Assert.True(test.Contains(3));
+        }
+
+        [Fact]
+        public void ValidatesContainMethodAndDoesNotFindElement()
+        {
+            var test = new IntArray();
+            test.Add(5);
+            test.Add(3);
+            Assert.False(test.Contains(2));
+        }
+
+        [Fact]
+        public void ValidatesIndexOfMethodAndReturnsIndex()
+        {
+            var test = new IntArray();
+            test.Add(5);
+            test.Add(3);
+            Assert.Equal(1, test.IndexOf(3));
+        }
+
+        [Fact]
+        public void ValidatesIndexOfMethodAndDoesNotReturnIndex()
+        {
+            var test = new IntArray();
+            test.Add(5);
+            test.Add(3);
+            Assert.Equal(-1, test.IndexOf(1000));
+        }
+
+        [Fact]
+        public void ValidatesInsertMethod()
+        {
+            var test = new IntArray();
+            test.Add(5);
+            test.Add(3);
+            test.Insert(0, 4);
+            Assert.Equal(4, test.Element(0));
+            Assert.Equal(5, test.Element(1));
+            Assert.Equal(3, test.Element(2));
+            Assert.Equal(3, test.Count());
+        }
+
+        [Fact]
+        public void ValidatesClearMethod()
+        {
+            var test = new IntArray();
+            test.Add(5);
+            test.Add(3);
+            test.Clear();
+            Assert.Equal(0, test.Count());
+        }
+
+        [Fact]
+        public void ValidatesRemoveMethod()
+        {
+            var test = new IntArray();
+            test.Add(5);
+            test.Add(3);
+            test.Remove(5);
+            Assert.Equal(3, test.Element(0));
+        }
+
+        [Fact]
+        public void ValidatesRemoveAtMethod()
+        {
+            var test = new IntArray();
+            test.Add(5);
+            test.Add(3);
+            test.RemoveAt(0);
+            Assert.Equal(3, test.Element(0));
+        }
+    }
+}
