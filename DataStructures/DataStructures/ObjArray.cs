a71a3e71e4f1178562f90138d934d29207526ad7
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2020-05-28 15:52:04 +0300

    ObjArray + tests

---
+++ DataStructures/DataStructures/ObjArray.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class ObjArray
+    {
+        private object[] array;
+
+        public ObjArray()
+        {
+            this.array = new object[4];
+        }
+
+        public int Count { get; private set; }
+
+        public virtual object this[int index]
+        {
+            get => array[index];
+            set => array[index] = value;
+        }
+
+        public virtual void Add(object element)
+        {
+            EnsureCapactity();
+            array[Count] = element;
+            Count++;
+        }
+
+        public bool Contains(object element)
+        {
+            return IndexOf(element) != -1;
+        }
+
+        public int IndexOf(object element)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (array[i] == element)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public virtual void Insert(int index, object element)
+        {
+            EnsureCapactity();
+            ShiftRight(index);
+            array[index] = element;
+            Count++;
+        }
+
+        public void Clear()
+        {
+            this.Count = 0;
+        }
+
+        public void Remove(object element)
+        {
+            int index = 0;
+            for (int i = 0; i < Count; i++)
+            {
+                if (array[i].Equals(element))
+                {
+                    index = i;
+                }
+            }
+
+            RemoveAt(index);
+        }
+
+        public void RemoveAt(int index)
+        {
+            ShiftLeft(index);
+            Count--;
+        }
+
+        protected void EnsureCapactity()
+        {
+            if (Count != array.Length)
+            {
+                return;
+            }
+
+            Array.Resize(ref array, array.Length * 2);
+        }
+
+        private void ShiftLeft(int index)
+        {
+            for (int i = Count - 1; i > index; i--)
+            {
+                array[i - 1] = array[i];
+            }
+        }
+
+        private void ShiftRight(int index)
+        {
+            for (int i = Count; i > index; i--)
+            {
+                array[i] = array[i - 1];
+            }
+        }
+    }
+}
---
+++ DataStructures/DataStructures/ObjArrayTests.cs
@@ -0,0 +1,134 @@
+﻿using Xunit;
+
+namespace DataStructures
+{
+    public class ObjArrayTests
+    {
+        [Fact]
+        public void ValidatesConstructor()
+        {
+            var test = new ObjArray();
+            Assert.Equal(0, test.Count);
+            Assert.Equal(0, test[0]);
+            Assert.Equal(0, test[1]);
+            Assert.Equal(0, test[2]);
+            Assert.Equal(0, test[3]);
+        }
+
+        [Fact]
+        public void ValidatesAddMethod()
+        {
+            var test = new ObjArray();
+            test.Add("Abc");
+            test.Add(3.02);
+            test.Add('E');
+            test.Add(6);
+            test.Add(7);
+            Assert.Equal("Abc", test[0]);
+            Assert.Equal(3.02, test[1]);
+            Assert.Equal('E', test[2]);
+            Assert.Equal(6, test[3]);
+            Assert.Equal(7, test[4]);
+        }
+
+        [Fact]
+        public void ValidatesCountMethod()
+        {
+            var test = new ObjArray();
+            test.Add(5);
+            test.Add(3);
+            test.Add(2);
+            test.Add(6);
+            test.Add(7);
+            Assert.Equal(5, test.Count);
+        }
+
+        [Fact]
+        public void ValidatesSetElementMethod()
+        {
+            var test = new ObjArray();
+            test.Add(5);
+            test[0] = 'c';
+            Assert.Equal('c', test[0]);
+        }
+
+        [Fact]
+        public void ValidatesContainMethodAndFindsElement()
+        {
+            var test = new ObjArray();
+            test.Add("Andrei");
+            test.Add(3);
+            Assert.True(test.Contains("Andrei"));
+        }
+
+        [Fact]
+        public void ValidatesContainMethodAndDoesNotFindElement()
+        {
+            var test = new ObjArray();
+            test.Add("Andrei");
+            test.Add(3);
+            Assert.False(test.Contains(2));
+        }
+
+        [Fact]
+        public void ValidatesIndexOfMethodAndReturnsIndex()
+        {
+            var test = new ObjArray();
+            test.Add(5);
+            test.Add('b');
+            Assert.Equal(1, test.IndexOf('b'));
+        }
+
+        [Fact]
+        public void ValidatesIndexOfMethodAndDoesNotReturnIndex()
+        {
+            var test = new ObjArray();
+            test.Add(5);
+            test.Add('b');
+            Assert.Equal(-1, test.IndexOf(1000));
+        }
+
+        [Fact]
+        public void ValidatesInsertMethod()
+        {
+            var test = new ObjArray();
+            test.Add(5);
+            test.Add(3);
+            test.Insert(0, "abc");
+            Assert.Equal("abc", test[0]);
+            Assert.Equal(5, test[1]);
+            Assert.Equal(3, test[2]);
+            Assert.Equal(3, test.Count);
+        }
+
+        [Fact]
+        public void ValidatesClearMethod()
+        {
+            var test = new ObjArray();
+            test.Add(5);
+            test.Add(3);
+            test.Clear();
+            Assert.Equal(0, test.Count);
+        }
+
+        [Fact]
+        public void ValidatesRemoveMethod()
+        {
+            var test = new ObjArray();
+            test.Add(5);
+            test.Add(3);
+            test.Remove(5);
+            Assert.Equal(3, test[0]);
+        }
+
+        [Fact]
+        public void ValidatesRemoveAtMethod()
+        {
+            var test = new ObjArray();
+            test.Add(5);
+            test.Add(3);
+            test.RemoveAt(0);
+            Assert.Equal(3, test[0]);
+        }
+    }
+}
