839288af2aeae80e9f47c1f6def183738f3e7b1f
Bejinariu Paul <bejinariucatalin29@gmail.com>
2020-11-27 02:22:52 +0200

    Stock class with tests, callback test not working

---
+++ LinqApplications/LinqApplications/Stock.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqApplications
+{
+    public class Stock
+    {
+        private readonly List<Product> stock;
+        private Action<Product, int> callback;
+
+        public Stock() : this(new List<Product>())
+        {
+        }
+
+        public Stock(List<Product> products)
+        {
+            stock = products;
+        }
+
+        public void AddCalback(Action<Product, int> action)
+        {
+            callback = action;
+        }
+
+        public void AddProducts(params Product[] toAdd)
+        {
+            foreach (var product in toAdd)
+            {
+                stock.Add(product);
+            }
+        }
+
+        public void AddProducts(string productName, int quantity)
+        {
+            Product toFind = stock.Single(element => element.Name == productName);
+            Product toReplace = new Product(toFind.Name, toFind.Quantity + quantity);
+            stock.Remove(toFind);
+            stock.Add(toReplace);
+        }
+
+        public int GetQuantity(Product product)
+        {
+            return stock.Single(x => x.Name == product.Name).Quantity;
+        }
+
+        public void Buy(string productName, int quantity)
+        {
+            Product toFind = stock.Single(element => element.Name == productName);
+            Product toReplace = new Product(toFind.Name, toFind.Quantity - quantity);
+            stock.Remove(toFind);
+            stock.Add(toReplace);
+            CallbackAction(toReplace, toFind.Quantity);
+        }
+
+        private bool CheckQuantity(int currentQuantity, int previousQuantity, int limit)
+        {
+            return previousQuantity >= limit && currentQuantity < limit;
+        }
+
+        private void CallbackAction(Product product, int previousQuantity)
+        {
+            if (callback == null || !CheckCallback(product, previousQuantity))
+            {
+                return;
+            }
+
+            callback(product, product.Quantity);
+        }
+
+        private bool CheckCallback(Product product, int previousQuantity)
+        {
+            int currentQuantity = product.Quantity;
+
+            return CheckQuantity(previousQuantity, currentQuantity, 10)
+                || CheckQuantity(previousQuantity, currentQuantity, 5)
+                || CheckQuantity(previousQuantity, currentQuantity, 2);
+        }
+    }
+}
---
+++ LinqApplications/LinqApplications/StockFacts.cs
@@ -0,0 +1,64 @@
+﻿using Xunit;
+using System.Collections.Generic;
+
+namespace LinqApplications
+{
+    public class StockFacts
+    {
+        [Fact]
+        public void AddProductAddsOneProduct()
+        {
+            var snickers = new Product("snickers", 15);
+            var croissant = new Product("croissant", 10);
+            var bagels = new Product("bagels", 20);
+            var products = new Stock(new List<Product> { snickers, croissant });
+            products.AddProducts(bagels);
+            Assert.Equal(20, products.GetQuantity(bagels));
+        }
+
+        [Fact]
+        public void AddQuantityToExistingProduct()
+        {
+            var snickers = new Product("snickers", 15);
+            var croissant = new Product("croissant", 10);
+            var products = new Stock(new List<Product> { snickers, croissant });
+            products.AddProducts("croissant", 5);
+            Assert.Equal(15, products.GetQuantity(croissant));
+        }
+
+        [Fact]
+        public void BuyShouldBuyOneProduct()
+        {
+            var snickers = new Product("snickers", 15);
+            var products = new Stock(new List<Product> { snickers });
+            products.Buy("snickers", 1);
+            Assert.Equal(14, products.GetQuantity(snickers));
+        }
+
+        [Fact]
+        public void BuyShouldBuyMultipleProduct()
+        {
+            var snickers = new Product("snickers", 15);
+            var products = new Stock(new List<Product> { snickers });
+            products.Buy("snickers", 5);
+            Assert.Equal(10, products.GetQuantity(snickers));
+        }
+
+        [Fact]
+        public void CallbackShouldBeCalledAfterBuy()
+        {
+            var products = new Stock(new List<Product> { new Product("snickers", 15) });
+            int testQuantity = 0;
+            Product testProduct = null;
+            void TestCallback(Product product, int quantity)
+            {
+                testProduct = product;
+                testQuantity = quantity;
+            }
+
+            products.AddCalback(TestCallback);
+            products.Buy("snickers", 6);
+            Assert.Equal(9, testQuantity);
+        }
+    }
+}
