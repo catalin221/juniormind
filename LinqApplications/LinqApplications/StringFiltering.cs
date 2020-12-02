efc65659c3b5eb012449a09117387c9fec5997a6
Bejinariu Paul <bejinariucatalin29@gmail.com>
2020-12-02 14:31:43 +0200

    Added FirstUniqueChar and MostOccurencesChar with tests

---
+++ LinqApplications/LinqApplications/StringFiltering.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqApplications
+{
+    public class StringFiltering
+    {
+        public static void CountVowelsAndConsonants(string word, ref int vowelsCount, ref int consonantsCount)
+        {
+            vowelsCount = word.Aggregate(0, (vowels, current) => "aeiouAEIOU".Contains(current) ? vowels + 1 : vowels);
+            consonantsCount = word.Aggregate(0, (consonants, current) => "aeiouAEIOU".Contains(current) ? consonants : consonants + 1);
+        }
+
+        public static char FindFirstUniqueCharacter(string word)
+        {
+            return word.GroupBy(x => x).First(element => element.Count() == 1).Key;
+        }
+
+        public static char FindCharacterWithMostOccurences(string word)
+        {
+            return word.GroupBy(element => element).Aggregate((max, element) => element.Count() > max.Count() ? element : max).Key;
+        }
+    }
+}
---
+++ LinqApplications/LinqApplications/StringFilteringFacts.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace LinqApplications
+{
+    public class StringFilteringFacts
+    {
+        [Fact]
+        public void CheckVowelsAndConsonantsNumber()
+        {
+            const string testWord = "abcdef";
+            int vowels = 0;
+            int consonants = 0;
+            StringFiltering.CountVowelsAndConsonants(testWord, ref vowels, ref consonants);
+            Assert.Equal((2, 4), (vowels, consonants));
+        }
+
+        [Fact]
+        public void CheckFirstUniqueCharcaterInString()
+        {
+            const string testWord = "cbabcea";
+            Assert.Equal('e', StringFiltering.FindFirstUniqueCharacter(testWord));
+        }
+
+        [Fact]
+        public void CheckFirstUniqueCharcaterInStringWithOnlyUniqueCharacters()
+        {
+            const string testWord = "cbae";
+            Assert.Equal('c', StringFiltering.FindFirstUniqueCharacter(testWord));
+        }
+
+        [Fact]
+        public void CheckMostOccurenciesCharInString()
+        {
+            const string testWord = "cbeabcca";
+            Assert.Equal('c', StringFiltering.FindCharacterWithMostOccurences(testWord));
+        }
+    }
+}
