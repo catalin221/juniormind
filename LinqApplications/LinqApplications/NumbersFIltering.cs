8d39d89247fcc43a6c8c9cbff9a2badb4cb21fb0
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2021-01-18 17:28:08 +0200

    Subsets of sum up to k implemented, with tests

---
+++ LinqApplications/LinqApplications/NumbersFIltering.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqApplications
+{
+    public class NumbersFiltering
+    {
+        public static IEnumerable<IEnumerable<int>> GetSubsetsOfSpecificSum(IEnumerable<int> source, int maxSum)
+        {
+            return source.SelectMany((x, index) => source.Skip(index)
+            .Select((number, secondIndex) => source
+            .Skip(secondIndex).Take(source.Count() - secondIndex - index))
+             .Where(x => x.Sum() <= maxSum));
+        }
+    }
+}
---
+++ LinqApplications/LinqApplications/NumbersFiletringFacts.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace LinqApplications
+{
+    public class NumbersFiletringFacts
+    {
+        [Fact]
+        public void GenerateSubsetsGeneratesSubsetsOfGivenSum()
+        {
+            var set = new[] { 1, 2, 3, 4, 5, 6 };
+            Assert.Equal(new[] { new[] { 4 }, new[] { 3 }, new[] { 1, 2 }, new[] { 2 }, new[] { 1 } }, NumbersFiltering.GetSubsetsOfSpecificSum(set, 4));
+        }
+    }
+}
