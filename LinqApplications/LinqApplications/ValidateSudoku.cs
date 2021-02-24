bb9b526f66bf75e9bf0e84d527ff001d34836ac6
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2021-02-24 18:36:41 +0200

    Implemented Sudoku validator using LINQ

---
+++ LinqApplications/LinqApplications/ValidateSudoku.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqApplications
+{
+    public static class ValidateSudoku
+    {
+        public static bool CheckSudoku(byte[][] square)
+        {
+            ThrowNullException(square);
+            int count = square.Length;
+            int squareSize = (int)Math.Sqrt(count);
+            var digits = Enumerable.Range(0, count);
+            var lines = square.Select(x => x);
+            var columns = digits.Select(index => square.Select(column => column[index]));
+            var squares = digits.Select(index => GetInnerBlock(square, index / squareSize * squareSize, index % squareSize * squareSize, squareSize));
+            var sudoku = lines.Concat(columns).Concat(squares);
+            return sudoku.All(y => CheckByteLine(y, count));
+        }
+
+        private static bool CheckByteLine(IEnumerable<byte> line, int count)
+        {
+            bool elementsIntegrity = line.Select(x => Convert.ToInt32(x)).All(x => Enumerable.Range(1, count).Contains(x));
+            return elementsIntegrity && line.Count() == line.Distinct().Count();
+        }
+
+        private static IEnumerable<byte> GetInnerBlock(byte[][] square, int lineIndex, int columnIndex, int count)
+        {
+            return Enumerable.Range(lineIndex, count).SelectMany(x => Enumerable.Range(columnIndex, count).Select(y => square[x][y]));
+        }
+
+        private static void ThrowNullException(byte[][] source)
+        {
+            if (source != null)
+            {
+                return;
+            }
+
+            throw new ArgumentNullException(nameof(source));
+        }
+    }
+}
---
+++ LinqApplications/LinqApplications/ValidateSudokuFacts.cs
@@ -0,0 +1,83 @@
+﻿using Xunit;
+
+namespace LinqApplications
+{
+    public class ValidateSudokuFacts
+    {
+        [Fact]
+        public void CheckValidSudoku()
+        {
+            byte[][] testSquare =
+            {
+                new byte[] { 4, 3, 5, 2, 6, 9, 7, 8, 1 },
+                new byte[] { 6, 8, 2, 5, 7, 1, 4, 9, 3 },
+                new byte[] { 1, 9, 7, 8, 3, 4, 5, 6, 2 },
+                new byte[] { 8, 2, 6, 1, 9, 5, 3, 4, 7 },
+                new byte[] { 3, 7, 4, 6, 8, 2, 9, 1, 5 },
+                new byte[] { 9, 5, 1, 7, 4, 3, 6, 2, 8 },
+                new byte[] { 5, 1, 9, 3, 2, 6, 8, 7, 4 },
+                new byte[] { 2, 4, 8, 9, 5, 7, 1, 3, 6 },
+                new byte[] { 7, 6, 3, 4, 1, 8, 2, 5, 9 }
+            };
+
+            Assert.True(ValidateSudoku.CheckSudoku(testSquare));
+        }
+
+        [Fact]
+        public void CheckInvalidSudokuOnFirstLine()
+        {
+            byte[][] testSquare =
+            {
+                new byte[] { 4, 3, 5, 2, 5, 9, 7, 8, 1 },
+                new byte[] { 6, 8, 2, 5, 7, 1, 4, 9, 3 },
+                new byte[] { 1, 9, 7, 8, 3, 4, 5, 6, 2 },
+                new byte[] { 8, 2, 6, 1, 9, 5, 3, 4, 7 },
+                new byte[] { 3, 7, 4, 6, 8, 2, 9, 1, 5 },
+                new byte[] { 9, 5, 1, 7, 4, 3, 6, 2, 8 },
+                new byte[] { 5, 1, 9, 3, 2, 6, 8, 7, 4 },
+                new byte[] { 2, 4, 8, 9, 5, 7, 1, 3, 6 },
+                new byte[] { 7, 6, 3, 4, 1, 8, 2, 5, 9 }
+            };
+
+            Assert.False(ValidateSudoku.CheckSudoku(testSquare));
+        }
+
+        [Fact]
+        public void CheckInvalidSudokuInFirstBlock()
+        {
+            byte[][] testSquare =
+            {
+                new byte[] { 4, 3, 5, 2, 6, 9, 7, 8, 1 },
+                new byte[] { 6, 3, 2, 5, 7, 1, 4, 9, 3 },
+                new byte[] { 1, 9, 7, 8, 3, 4, 5, 6, 2 },
+                new byte[] { 8, 2, 6, 1, 9, 5, 3, 4, 7 },
+                new byte[] { 3, 7, 4, 6, 8, 2, 9, 1, 5 },
+                new byte[] { 9, 5, 1, 7, 4, 3, 6, 2, 8 },
+                new byte[] { 5, 1, 9, 3, 2, 6, 8, 7, 4 },
+                new byte[] { 2, 4, 8, 9, 5, 7, 1, 3, 6 },
+                new byte[] { 7, 6, 3, 4, 1, 8, 2, 5, 9 }
+            };
+
+            Assert.False(ValidateSudoku.CheckSudoku(testSquare));
+        }
+
+        [Fact]
+        public void CheckInvalidSudokuOnFirstColumn()
+        {
+            byte[][] testSquare =
+            {
+                new byte[] { 4, 3, 5, 2, 6, 9, 7, 8, 1 },
+                new byte[] { 6, 8, 2, 5, 7, 1, 4, 9, 3 },
+                new byte[] { 1, 9, 7, 8, 3, 4, 5, 6, 2 },
+                new byte[] { 8, 2, 6, 1, 9, 5, 3, 4, 7 },
+                new byte[] { 4, 7, 4, 6, 8, 2, 9, 1, 5 },
+                new byte[] { 9, 5, 1, 7, 4, 3, 6, 2, 8 },
+                new byte[] { 5, 1, 9, 3, 2, 6, 8, 7, 4 },
+                new byte[] { 2, 4, 8, 9, 5, 7, 1, 3, 6 },
+                new byte[] { 7, 6, 3, 4, 1, 8, 2, 5, 9 }
+            };
+
+            Assert.False(ValidateSudoku.CheckSudoku(testSquare));
+        }
+    }
+}
