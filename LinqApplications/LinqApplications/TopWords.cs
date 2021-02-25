73a5af3503e6d4b8e8fe3533233bc735e3a186f2
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2021-02-25 19:21:08 +0200

    Most apparitions of words in text implemented

---
+++ LinqApplications/LinqApplications/TopWords.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqApplications
+{
+    public static class TopWords
+    {
+        public static IEnumerable<string> GetTopWords(string input, int topWords)
+        {
+            ThrowNullException(input);
+            return input.Split(new[] { ' ', '.', ',', ';', ':' }, StringSplitOptions.RemoveEmptyEntries)
+                .GroupBy(x => x.ToLower()).OrderByDescending(x => x.Count())
+                .Select(x => string.Join(" - ", x.Key, x.Count().ToString())).Take(topWords);
+        }
+
+        private static void ThrowNullException(string source)
+        {
+            if (source != null)
+            {
+                return;
+            }
+
+            throw new ArgumentNullException(nameof(source));
+        }
+    }
+}
---
+++ LinqApplications/LinqApplications/TopWordsFacts.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using System.Collections.Generic;
+
+namespace LinqApplications
+{
+    public class TopWordsFacts
+    {
+        [Fact]
+        public void GetTopWordsReturnsMostUsedWords()
+        {
+            const string testText = "string split splits a string";
+            Assert.Equal(new List<string> { "string - 2", "split - 1" }, TopWords.GetTopWords(testText, 2));
+        }
+
+        [Fact]
+        public void GetTopWordsKeepsOrderForSingularElements()
+        {
+            const string testText = "string split splits a";
+            Assert.Equal(new List<string> { "string - 1", "split - 1", "splits - 1", "a - 1" }, TopWords.GetTopWords(testText, 4));
+        }
+
+        [Fact]
+        public void GetTopWordsSplitsBySeparatorsCorrectly()
+        {
+            const string testText = "string. split, splits; a";
+            Assert.Equal(new List<string> { "string - 1", "split - 1", "splits - 1", "a - 1" }, TopWords.GetTopWords(testText, 4));
+        }
+    }
+}
