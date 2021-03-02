b872af0766bd316540d0e89fb4e222bdb528e2a5
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2021-03-02 14:02:36 +0200

    ReversePolishNotation implemented using LINQ

---
+++ LinqApplications/LinqApplications/ReversePolishNotation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqApplications
+{
+    public static class ReversePolishNotation
+    {
+        public static double CalculateExpression(string expression)
+        {
+            ThrowNullException(expression);
+
+            return expression.Split().Aggregate(Enumerable.Empty<double>(), (operands, current) => IsOperator(current)
+                                    ? UpdateResult(operands, current, 2)
+                                    : operands.Append(Convert.ToDouble(current))).First();
+        }
+
+        private static IEnumerable<double> UpdateResult(IEnumerable<double> operands, string current, int skip)
+        {
+            return operands.SkipLast(skip).Append(ApplyOperator(operands.TakeLast(skip), current));
+        }
+
+        private static double ApplyOperator(IEnumerable<double> operands, string operation)
+        {
+            double firstOperand = operands.First();
+            double secondOperand = operands.Last();
+            return operation switch
+            {
+                "+" => firstOperand + secondOperand,
+                "-" => firstOperand - secondOperand,
+                "*" => firstOperand * secondOperand,
+                "/" => firstOperand / secondOperand,
+            };
+        }
+
+        private static bool IsOperator(string element)
+        {
+            return "+-*/".Contains(element);
+        }
+
+        private static void ThrowNullException(string source)
+        {
+            if (source != null)
+            {
+                return;
+            }
+
+            throw new ArgumentNullException(nameof(source));
+        }
+    }
+}
---
+++ LinqApplications/LinqApplications/ReversePolishNotationFacts.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace LinqApplications
+{
+    public class ReversePolishNotationFacts
+    {
+        [Fact]
+        public void CalculateReversePolishNotationForSimpleExpression()
+        {
+            Assert.Equal(5, ReversePolishNotation.CalculateExpression("7 5 - 3 +"));
+        }
+
+        [Fact]
+        public void CalculateReversePolishNotationForComplexExpression()
+        {
+            Assert.Equal(5, ReversePolishNotation.CalculateExpression("15 7 1 1 + - / 3 * 2 1 1 + + -"));
+        }
+    }
+}
