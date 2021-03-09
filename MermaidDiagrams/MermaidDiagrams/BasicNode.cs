bc26d418f6df8e8833d452b248f425a0f7f76841
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2021-03-09 19:28:23 +0200

    Fixed spacing for text

--- MermaidDiagrams/MermaidDiagrams/BasicNode.cs
+++ MermaidDiagrams/MermaidDiagrams/BasicNode.cs
@@ -13,7 +13,7 @@
         {
             const string rectangle = "<rect width =\"70\" height =\"40\" x =\"1\" y =\"100\" " +
                                      "style = \"fill:rgb(195, 236, 168) ; stroke-width:2; stroke: rgb(0, 0, 0)\" />";
-            string text = "<text x =\"25\" y=\"120\" fill =\"black\" >" + id + "</ text >";
+            string text = "<text x =\"25\" y=\"120\" fill =\"black\" >" + id + "</text >";
             return rectangle + text;
         }
     }
