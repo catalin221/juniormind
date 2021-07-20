36ae015dedf61f5e670e3d09a915487af63f4908
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2021-07-20 17:20:22 +0300

    Updated Draw class for UserJourney
    Also split UserDot in two for individual drawing

--- MermaidDiagrams/UserJourney/DrawUserJourney.cs
+++ MermaidDiagrams/UserJourney/DrawUserJourney.cs
@@ -25,29 +25,30 @@ namespace UserJourney
 
         public void DrawJourney()
         {
-            string[] colors = new[]
+            string[] rectangleColors = new[]
             {
                 "230, 104, 50", "230, 149, 50", "242, 219, 87", "230, 252, 81", "164, 250, 15", "25, 29, 255", "121, 58, 240",
                 "138, 65, 217", "171, 65, 217", "199, 65, 217", "217, 65, 194", "217, 65, 113", "217, 65, 65"
             };
 
+            string[] userDotColors = new[] { "90, 252, 3", "6, 194, 112", "203, 235, 190", "198, 190, 235", "231, 235, 190", "235, 208, 190" };
+
             int colorIndex = -1;
             int numberOfActions = 0;
             foreach (var section in sections)
             {
-                var sectionRectangle = new SectionRectangle(section.Key, colors[colorIndex++]);
-                sectionRectangle.UpdateCoordinates(sectionCoords);
-                sectionRectangle.Draw();
+                DrawSectionRectangle(section, rectangleColors, ref colorIndex);
                 ActionRectangle lastAction = new ActionRectangle("null", "null");
 
                 foreach (var action in section.Value)
                 {
-                    var actionRectangle = new ActionRectangle(action.actionName, colors[colorIndex]);
-                    actionRectangle.UpdateCoordinates(actionCoords);
-                    actionRectangle.Draw();
+                    DrawActionRectangle(action, rectangleColors, colorIndex, out ActionRectangle actionRectangle);
                     UpdateActionCoordinates(actionRectangle.GetDimensions().width);
                     numberOfActions++;
                     lastAction = actionRectangle;
+                    foreach (string user in action.users)
+                    {
+                    }
                 }
 
                 UpdateSectionCoordinates(lastAction.GetDimensions().width);
@@ -56,6 +57,27 @@ namespace UserJourney
             Arrow drawArrow = new Arrow((numberOfActions * 150) + (numberOfActions * 20));
             drawArrow.UpdateCoordinates(arrowCoords);
             UpdateBackground(drawArrow.GetDimensions());
+        }
+
+        private void DrawActionRectangle((string actionName, int score, List<string> users) action, string[] rectangleColors, int colorIndex, out ActionRectangle actionRectangle)
+        {
+            actionRectangle = new ActionRectangle(action.actionName, rectangleColors[colorIndex]);
+            actionRectangle.UpdateCoordinates(actionCoords);
+            actionRectangle.Draw();
+        }
+
+        private void DrawSectionRectangle(KeyValuePair<string, List<(string actionName, int score, List<string> users)>> section, string[] rectangleColors, ref int colorIndex)
+        {
+            var sectionRectangle = new SectionRectangle(section.Key, rectangleColors[colorIndex++]);
+            sectionRectangle.UpdateCoordinates(sectionCoords);
+            sectionRectangle.Draw();
+        }
+
+        private void DrawDot(string color, (int x, int y) coordinates)
+        {
+            var dot = new UserDot(color);
+            dot.UpdateCoordinates(coordinates);
+            dot.Draw();
         }
 
         private void UpdateSectionCoordinates(int width)
--- MermaidDiagrams/UserJourney/UserDotId.cs
+++ MermaidDiagrams/UserJourney/UserDotId.cs
@@ -1,19 +1,23 @@
 ﻿namespace UserJourney
 {
-    public class UserDot : IUserJourney
+    public class UserDotId : IUserJourney
     {
         private readonly string color;
+        private readonly string id;
         private readonly (int width, int height) dimensions;
         private (int x, int y) coordinates;
 
-        public UserDot(string color)
+        public UserDotId(string id, string color)
         {
+            this.id = id;
             this.color = color;
         }
 
         public string Draw()
         {
-            return "<circle cx=\"" + coordinates.x + "\" cy=\"" + coordinates.y + "\" r=\"7\" stroke=\"black\" stroke-width=\"1\" fill=\"(" + color + ")\" /> ";
+            (int x, int y) textCoord = (coordinates.x + 20, coordinates.y + 5);
+            return "<circle cx=\"" + coordinates.x + "\" cy=\"" + coordinates.y + "\" r=\"7\" stroke=\"black\" stroke-width=\"1\" fill=\"(" + color + ")\" /> " +
+                "<text x=\"" + textCoord.x + "\" y=\"" + textCoord.y + "\" font-size=\"14\">" + id + "</text>\"";
         }
 
         public (int width, int height) GetDimensions()
